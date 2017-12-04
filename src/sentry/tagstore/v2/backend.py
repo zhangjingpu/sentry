da2b84da03ce5661325b3363b0e8abc850c19378
Brett Hoerner <brett@bretthoerner.com>
2017-12-04 15:05:47 -0600

    feat(tags): Rest of v2 backend

--- src/sentry/tagstore/v2/backend.py
+++ src/sentry/tagstore/v2/backend.py
@@ -8,10 +8,20 @@ sentry.tagstore.v2.backend
 
 from __future__ import absolute_import
 
-from django.db import IntegrityError, transaction
+import six
 
+from collections import defaultdict
+from datetime import timedelta
+from django.db import connections, router, IntegrityError, transaction
+from django.db.models import Q, Sum
+from django.utils import timezone
+from operator import or_
+from six.moves import reduce
+
+from sentry import buffer
 from sentry.tagstore import TagKeyStatus
 from sentry.tagstore.base import TagStorage
+from sentry.utils import db
 
 from .models import EventTag, GroupTagKey, GroupTagValue, TagKey, TagValue
 
@@ -307,6 +317,300 @@ class TagStorage(TagStorage):
         GroupTagValue.objects.filter(
             group_id=group_id,
         ).delete()
+
+    def incr_tag_key_values_seen(self, project_id, environment_id, key, count=1):
+        buffer.incr(TagKey,
+                    columns={
+                        'values_seen': count,
+                    },
+                    filters={
+                        'project_id': project_id,
+                        'key': key,
+                    })
+
+    def incr_tag_value_times_seen(self, project_id, environment_id,
+                                  key, value, extra=None, count=1):
+        buffer.incr(TagValue,
+                    columns={
+                        'times_seen': count,
+                    },
+                    filters={
+                        'project_id': project_id,
+                        'key': key,
+                        'value': value,
+                    },
+                    extra=extra)
+
+    def incr_group_tag_key_values_seen(self, project_id, group_id, environment_id, key, count=1):
+        buffer.incr(GroupTagKey,
+                    columns={
+                        'values_seen': count,
+                    },
+                    filters={
+                        'project_id': project_id,
+                        'group_id': group_id,
+                        'key': key,
+                    })
+
+    def incr_group_tag_value_times_seen(self, project_id, group_id, environment_id,
+                                        key, value, extra=None, count=1):
+        buffer.incr(GroupTagValue,
+                    columns={
+                        'times_seen': count,
+                    },
+                    filters={
+                        'group_id': group_id,
+                        'key': key,
+                        'value': value,
+                    },
+                    extra=extra)
+
+    def get_group_event_ids(self, project_id, group_id, environment_id, tags):
+        tagkeys = dict(
+            TagKey.objects.filter(
+                project_id=project_id,
+                key__in=tags.keys(),
+                status=TagKeyStatus.VISIBLE,
+            ).values_list('key', 'id')
+        )
+
+        tagvalues = {
+            (t[1], t[2]): t[0]
+            for t in TagValue.objects.filter(
+                reduce(or_, (Q(key=k, value=v)
+                             for k, v in six.iteritems(tags))),
+                project_id=project_id,
+            ).values_list('id', 'key', 'value')
+        }
+
+        try:
+            tag_lookups = [(tagkeys[k], tagvalues[(k, v)])
+                           for k, v in six.iteritems(tags)]
+            # [(1, 10), ...]
+        except KeyError:
+            # one or more tags were invalid, thus the result should be an empty
+            # set
+            return []
+
+        # Django doesnt support union, so we limit results and try to find
+        # reasonable matches
+
+        # get initial matches to start the filter
+        k, v = tag_lookups.pop()
+        matches = list(
+            EventTag.objects.filter(
+                key_id=k,
+                value_id=v,
+                group_id=group_id,
+            ).values_list('event_id', flat=True)[:1000]
+        )
+
+        # for each remaining tag, find matches contained in our
+        # existing set, pruning it down each iteration
+        for k, v in tag_lookups:
+            matches = list(
+                EventTag.objects.filter(
+                    key_id=k,
+                    value_id=v,
+                    event_id__in=matches,
+                    group_id=group_id,
+                ).values_list('event_id', flat=True)[:1000]
+            )
+            if not matches:
+                return []
+
+        return matches
+
+    def get_group_values_seen(self, group_ids, environment_id, key):
+        if isinstance(group_ids, six.integer_types):
+            qs = GroupTagKey.objects.filter(group_id=group_ids)
+        else:
+            qs = GroupTagKey.objects.filter(group_id__in=group_ids)
+
+        return defaultdict(int, qs.filter(
+            key=key,
+        ).values_list('group_id', 'values_seen'))
+
+    def get_group_tag_value_count(self, group_id, environment_id, key):
+        if db.is_postgres():
+            # This doesnt guarantee percentage is accurate, but it does ensure
+            # that the query has a maximum cost
+            using = router.db_for_read(GroupTagValue)
+            cursor = connections[using].cursor()
+            cursor.execute(
+                """
+                SELECT SUM(t)
+                FROM (
+                    SELECT times_seen as t
+                    FROM sentry_messagefiltervalue
+                    WHERE group_id = %s
+                    AND key = %s
+                    ORDER BY last_seen DESC
+                    LIMIT 10000
+                ) as a
+            """, [group_id, key]
+            )
+            return cursor.fetchone()[0] or 0
+
+        cutoff = timezone.now() - timedelta(days=7)
+        return GroupTagValue.objects.filter(
+            group_id=group_id,
+            key=key,
+            last_seen__gte=cutoff,
+        ).aggregate(t=Sum('times_seen'))['t']
+
+    def get_top_group_tag_values(self, group_id, environment_id, key, limit=3):
+        if db.is_postgres():
+            # This doesnt guarantee percentage is accurate, but it does ensure
+            # that the query has a maximum cost
+            return list(
+                GroupTagValue.objects.raw(
+                    """
+                SELECT *
+                FROM (
+                    SELECT *
+                    FROM sentry_messagefiltervalue
+                    WHERE group_id = %%s
+                    AND key = %%s
+                    ORDER BY last_seen DESC
+                    LIMIT 10000
+                ) as a
+                ORDER BY times_seen DESC
+                LIMIT %d
+            """ % limit, [group_id, key]
+                )
+            )
+
+        cutoff = timezone.now() - timedelta(days=7)
+        return list(
+            GroupTagValue.objects.filter(
+                group_id=group_id,
+                key=key,
+                last_seen__gte=cutoff,
+            ).order_by('-times_seen')[:limit]
+        )
+
+    def get_first_release(self, group_id):
+        try:
+            first_release = GroupTagValue.objects.filter(
+                group_id=group_id,
+                key__in=('sentry:release', 'release'),
+            ).order_by('first_seen')[0]
+        except IndexError:
+            return None
+        else:
+            return first_release.value
+
+    def get_last_release(self, group_id):
+        try:
+            last_release = GroupTagValue.objects.filter(
+                group_id=group_id,
+                key__in=('sentry:release', 'release'),
+            ).order_by('-last_seen')[0]
+        except IndexError:
+            return None
+
+        return last_release.value
+
+    def get_release_tags(self, project_ids, environment_id, versions):
+        return list(TagValue.objects.filter(
+            project_id__in=project_ids,
+            key='sentry:release',
+            value__in=versions,
+        ))
+
+    def get_group_ids_for_users(self, project_ids, event_users, limit=100):
+        return list(GroupTagValue.objects.filter(
+            key='sentry:user',
+            value__in=[eu.tag_value for eu in event_users],
+            project_id__in=project_ids,
+        ).order_by('-last_seen').values_list('group_id', flat=True)[:limit])
+
+    def get_group_tag_values_for_users(self, event_users, limit=100):
+        tag_filters = [Q(value=eu.tag_value, project_id=eu.project_id) for eu in event_users]
+        return list(GroupTagValue.objects.filter(
+            reduce(or_, tag_filters),
+            key='sentry:user',
+        ).order_by('-last_seen')[:limit])
+
+    def get_tags_for_search_filter(self, project_id, tags):
+        from sentry.search.base import ANY, EMPTY
+        # Django doesnt support union, so we limit results and try to find
+        # reasonable matches
+
+        # ANY matches should come last since they're the least specific and
+        # will provide the largest range of matches
+        tag_lookups = sorted(six.iteritems(tags), key=lambda x: x != ANY)
+
+        # get initial matches to start the filter
+        matches = None
+
+        # for each remaining tag, find matches contained in our
+        # existing set, pruning it down each iteration
+        for k, v in tag_lookups:
+            if v is EMPTY:
+                return None
+
+            elif v != ANY:
+                base_qs = GroupTagValue.objects.filter(
+                    key=k,
+                    value=v,
+                    project_id=project_id,
+                )
+
+            else:
+                base_qs = GroupTagValue.objects.filter(
+                    key=k,
+                    project_id=project_id,
+                ).distinct()
+
+            if matches:
+                base_qs = base_qs.filter(group_id__in=matches)
+            else:
+                # restrict matches to only the most recently seen issues
+                base_qs = base_qs.order_by('-last_seen')
+
+            matches = list(base_qs.values_list('group_id', flat=True)[:1000])
+
+            if not matches:
+                return None
+
+        return matches
+
+    def update_group_tag_key_values_seen(self, project_id, group_ids):
+        gtk_qs = GroupTagKey.objects.filter(
+            project_id=project_id,
+            group_id__in=group_ids
+        )
+
+        for instance in gtk_qs:
+            instance.update(
+                values_seen=GroupTagValue.objects.filter(
+                    project_id=instance.project_id,
+                    group_id=instance.group_id,
+                    key=instance.key,
+                ).count(),
+            )
+
+    def get_tag_value_qs(self, project_id, environment_id, key, query=None):
+        queryset = TagValue.objects.filter(
+            project_id=project_id,
+            key=key,
+        )
+
+        if query:
+            queryset = queryset.filter(value__contains=query)
+
+        return queryset
+
+    def get_group_tag_value_qs(self, group_id, environment_id, key):
+        return GroupTagValue.objects.filter(
+            group_id=group_id,
+            key=key,
+        )
+
+    def get_event_tag_qs(self, **kwargs):
+        return EventTag.objects.filter(**kwargs)
 
     def _get_environment_filter(self, environment_id):
         if environment_id is None:
